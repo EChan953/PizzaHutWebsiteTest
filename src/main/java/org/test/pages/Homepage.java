fff494c15ec70fa97f39d13273c56ea790305457
ccajoles <ccajoles@vertere-gs.com>
2026-06-19 15:45:41 +0800

    homepage

--- src/main/java/org/test/pages/Homepage.java
+++ src/main/java/org/test/pages/Homepage.java
@@ -11,8 +11,15 @@ public class Homepage {
     }
 
     private By registerPageBtn = By.cssSelector("[data-tag='register-lbl']");
+    private By loginButton = By.linkText("Login");
+    private By homeBanner = By.cssSelector("div[class='container-bg-top container-bg-top-mobile d-block d-lg-none wow fadeIn']");
+
+
 
     public void clickRegisterPageButton() {
         driver.findElement(registerPageBtn).click();
+    }
+    public void clickLoginPageButton() {
+        driver.findElement(loginButton).click();
     }
 }
